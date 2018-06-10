170104392b2e6318ea2cd282c1e9e0a316acc354
ihara0603 <ihara0603@gmail.com>
2018-06-10 16:31:16 +0900

    改行コードをカウントするだけでは、誤って改行コードのみの行を一行とカウントしてしまう可能性がある。 正規表現を用いて、行末に文字がある行数を数える方法へ変更。

--- LanguageKnock_10/__init__.py
+++ LanguageKnock_10/__init__.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 
 import codecs
+import re
 ifile = codecs.open('D:\hightemp.txt', 'r','utf-8')
 
 lines=ifile.read()
-
-print(lines.count('\n'))
 ifile.close()
+reg=re.findall('.$', lines, re.MULTILINE)
+
+print(len(reg))
+#print(lines.count('\n'))
